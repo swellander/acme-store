bd19135167e887d35c1753066606296c163f73d8
Sam Wellander <samwellander@gmail.com>
2018-10-10 13:04:54 -0400

    setup initial products reducer

---
+++ src/store/products.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+//ACTION TYPES
+const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
+
+//ACTION CREATORS
+const loadProducts = products => ({
+  type: LOAD_PRODUCTS,
+  products
+});
+
+//THUNK CREATORS
+export const _loadProducts = () => dispatch => (
+  axios.get('/api/products')
+    .then(response => response.data)
+    .then(products => {
+      const action = loadProducts(products);
+      dispatch(action);
+    })
+    .catch(err => {
+      throw err;
+    })
+)
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case LOAD_PRODUCTS:
+      console.log(action.products);
+      return action.products;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
