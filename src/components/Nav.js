7829c85af2102d60f409debe9f45c4b7d42c1818
Sam Wellander <samwellander@gmail.com>
2018-10-11 12:32:50 -0400

    add counters to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class Nav extends Component {
-  render = () => (
-    <div>
-      <Link to="/cart">Cart</Link>
-      <Link to="/orders">Orders</Link>
-    </div>
-  )
+class Nav extends Component {
+  render = () => {
+    const { cartSize, numOrders } = this.props;
+    return (
+      <div>
+        <Link to="/cart">Cart({cartSize})</Link>
+        <Link to="/orders">Orders({numOrders})</Link>
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = ({ orders }) => {
+  const cart = orders.find(order => order.status == 'CART');
+  return {
+    cartSize: cart ? cart.lineItems.reduce((total, item) => total + item.quantity, 0) : 0,
+    numOrders: orders.length
+  }
+}
+
+export default connect(mapStateToProps)(Nav);
